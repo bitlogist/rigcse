44d309fcc49d8aa1005797e2bf2ac24b2b5d8b1a
Bit Logist <thebitlogist@gmail.com>
2024-03-15 00:09:44 +0000

    added input option for sme hacker url

--- src/routes/tools/savemyexams/+page.svelte
+++ src/routes/tools/savemyexams/+page.svelte
@@ -2,14 +2,23 @@
   import Heading from '$lib/components/Heading.svelte'
   import Layout from '$lib/components/Layout.svelte'
   import { fetchQuestions } from '$lib/utils/savemyexams'
-  import { onMount } from 'svelte'
+  import { type AutocompleteOption, Autocomplete } from '@skeletonlabs/skeleton';
 
-  let url = 'https://www.savemyexams.com/igcse/biology/cie/23/topic-questions/1-characteristics-and-classification-of-living-organisms/1-1-characteristics-classification-and-features-of-organisms/-/multiple-choice-questions/medium/'
+  let url = ''
+  const options: AutocompleteOption<string>[] = []
+  
+  function onSelection(event: CustomEvent<AutocompleteOption<string>>): void {
+    url = event.detail.label
+  }
 </script>
 
 <Layout>
   <Heading>Save My Exams</Heading>
   <p>You can paste a Save My Exams topic question like <a href="https://www.savemyexams.com/igcse/biology/cie/23/topic-questions/1-characteristics-and-classification-of-living-organisms/1-1-characteristics-classification-and-features-of-organisms/-/multiple-choice-questions/medium/">this</a> into the input box to get free answers to any topic question.</p>
+  <input class="input" type="search" name="demo" bind:value={url} placeholder="Search..." />
+  <div class="card w-full max-w-sm max-h-48 p-4 overflow-y-auto" tabindex="-1">
+    <Autocomplete bind:input={url} {options} on:selection={onSelection} />
+  </div>
   {#await fetchQuestions(url)}
     Loading...
   {:then questions}
